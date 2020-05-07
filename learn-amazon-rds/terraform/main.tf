df142ea0517efbb5528e2e78aa88286927d14062
Joe <joe@Arendt.localdomain>
2020-05-07 17:06:22 +0100

    fixed azs, dbname and ami in main.tf for rds@

--- learn-amazon-rds/terraform/main.tf
+++ learn-amazon-rds/terraform/main.tf
@@ -70,7 +70,7 @@ module "vpc" {
 
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "euwest-1c"]
+  azs             = ["eu-west-1a", "eu-west-1b", "euw1-az3"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -106,7 +106,7 @@ module "db" {
   allocated_storage = 10
   storage_encrypted = false
 
-  name     = "wordpress-db"
+  name     = "wordpressdb"
   username = "mysqladmin"
   password = "acrypticpassword"
   port     = "3306"
@@ -171,15 +171,15 @@ module "ec2" {
   name                   = "wordpress"
   instance_count         = 1
 
-  ami                    = "ami-ebd02392"
+  ami                    = "ami-0dad359ff462124ca"
   instance_type          = "t2.micro"
-  key_name               = "user1"
+  key_name               = "releaseworks-academy"
   monitoring             = true
   vpc_security_group_ids = [aws_security_group.ec2-sg.id]
   subnet_id              = module.vpc.public_subnets.1
 
   tags = {
-    Terraform   = "true"
+    Name        = "wordpress1"
     Environment = "dev"
   }
 }
